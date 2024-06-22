45956d66360ee7f009322496ca921565dee080f3
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-22 12:46:52 -0400

    Authentication working now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Threads</h1>
-    </main>
-  );
+    <div>
+      {/* The user button which controls account information */}
+      <ClerkProvider>
+        <UserButton afterSignOutUrl="/" />
+      </ClerkProvider>
+    </div>
+  )
 }
